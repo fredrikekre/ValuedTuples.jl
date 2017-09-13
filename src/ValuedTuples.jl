bc4177759fce64b21630907ce067612caf2ca299
Brandon Taylor <brandon.taylor221@gmail.com>
2017-09-13 13:58:44 -0400

    everything changed

--- src/ValuedTuples.jl
+++ src/ValuedTuples.jl
@@ -1,16 +1,49 @@
 module ValuedTuples
 
 import MacroTools
+import FastClosures
+import FastClosures.@closure
+import Base.tail
+import Base.mapfoldr
+import Base._empty_reduce_error
+import Base.mapreduce
+import Base.eltype
+import Base.&
+import Base.|
+import Base.!
+import Base.ifelse
+import Base.getindex
+import Base.setindex
+import Base.merge
 
 include("tuple_algebra.jl")
 
 export ValuedTuple
 """
+    struct ValuedTuple{T, N}
+        tuple::T
+        names::N
+    end
+
+A valued tuple can be indexed only with `Val`s (create
+with [`@value`](@ref) or [`@values`](@ref)). Valued tuples can be manipulated in
+a type-stable way because the names are directly encoded into the type. You can
+use repeated values. `getindex` will take the last match when trying to index at
+a repeated value; for all matches, use [`match_index`](@ref) instead. A vector of
+tuples with consistent names will conveniently print as a markdown table.
+
 ```jldoctest
 julia> using ValuedTuples
 
-julia> ValuedTuple((1, 2, 3), @values a b c)
-(@VT a = 1 b = 2 c = 3)
+julia> v = ValuedTuple((1, 2.0, "3"), @values a b a)
+ValuedTuples.ValuedTuple(a = 1, b = 2.0, a = 3)
+
+julia> v[@value a]
+3
+
+julia> v[@value c]
+ERROR: Value Val{:c}() not found
+[...]
 
 julia> ValuedTuple((1, 2, 3), @values a)
 ERROR: Tuple size mismatch: leftovers (2, 3)
@@ -25,88 +58,18 @@ ERROR: All names must be Vals
 [...]
 ```
 """
-struct ValuedTuple{T <: Tuple, N <: Tuple}
+struct ValuedTuple{T, N}
     tuple::T
     names::N
-    ValuedTuple{T, N}(t, n) where {T, N} = begin
-        same_length(t, n)
-        if inner_value(all_tuple(is_value.(n)))
-            new(t, n)
-        else
-            error("All names must be Vals")
-        end
-    end
 end
 
-ValuedTuple(t::T, n::N) where {T <: Tuple, N <: Tuple} = ValuedTuple{T, N}(t, n)
-
 Base.show(io::IO, v::ValuedTuple) =
-    print(io, string("(@VT ", join([map(v.names, v.tuple) do name, item
-        "$(inner_value(name)) = $(repr(item))"
-    end...], " "), ")"))
+    print(io, "$ValuedTuple(" * join(map(v.names, v.tuple) do value, item
+        "$(devalue(value)) = $item"
+    end, ", ") * ")")
 
-decompose_assignment(e) =
-    MacroTools.@match e begin
-        ( key_ = value_ ) => (key, value)
-        s_Symbol => (s, s)
-        any_ => error("Unable to decompose assignment $any")
-    end
-
-export @VT
-"""
-    @VT args...
-
-Make a `ValuedTuple`. A valued tuple can be indexed only with `Val`s (create
-with [`@value`](@ref)). Valued tuples can be manipulated in a type-stable way
-because the names are directly encoded into the type. You can use repeated
-values. `getindex` will take the last match when trying to index at a repeated
-value; for all matches, use [`match_index`](@ref) instead. A vector of
-tuples with consistent names will conveniently print as a markdown table.
-
-```jldoctest
-julia> using ValuedTuples
-
-julia> b = 2;
-
-julia> v = @VT a = 1 b a = 3
-(@VT a = 1 b = 2 a = 3)
-
-julia> v[@value b]
-2
-
-julia> v[@value d]
-ERROR: BoundsError: attempt to access ()
-[...]
-
-julia> v[@value a]
-3
-
-julia> @VT x * y
-ERROR: Unable to decompose assignment x * y
-[...]
-
-julia> [(@VT a = 1 b = 2), (@VT b = 2 c = 3)]
-2-element Array{ValuedTuples.ValuedTuple{Tuple{Int64,Int64},N} where N<:Tuple,1}:
- (@VT a = 1 b = 2)
- (@VT b = 2 c = 3)
-
-julia> t = [(@VT a = 1 b = 2), (@VT a = 3 b = 4)]
-| a   | b   |
-|:--- |:--- |
-| 1   | 2   |
-| 3   | 4   |
-```
-"""
-macro VT(args...)
-    decomposed = decompose_assignment.(args)
-    Expr(:call, ValuedTuple,
-        Expr(:tuple, map(decomposed) do pair
-            pair[2]
-        end...),
-        map(decomposed) do pair
-            Val{pair[1]}()
-        end
-    ) |> esc
+which_index(v, value) = @closure1 map(v.names) do name
+    Val(name == value)
 end
 
 export match_index
@@ -116,22 +79,34 @@ export match_index
 ```jldoctest
 julia> using ValuedTuples
 
-julia> v = @VT a = 1 b = 2 a = 3
-(@VT a = 1 b = 2 a = 3)
+julia> v = ValuedTuple((1, 2, 3), @values a b c)
+ValuedTuples.ValuedTuple(a = 1, b = 2, c = 3)
 
 julia> match_index(v, @value a)
 (1, 3)
 ```
 """
-match_index(v::ValuedTuple, value) =
-    get_index(
-        v.tuple,
-        map(v.names) do name
-            same_type(name, value)
-        end)
+match_index(v, value) = v.tuple[which_index(v, value)]
+
+last_error(x, value) = last(x)
+last_error(x::Tuple{}, value) = error("Value $value not found")
 
 Base.getindex(v::ValuedTuple, value) =
-    last(match_index(v, value))
+    last_error((match_index(v, value)), value)
+
+Base.getindex(v::ValuedTuple, names::Tuple) =
+    @closure1 map(names) do name
+        v[name]
+    end
+
+with(f, v::ValuedTuple) = ValuedTuple(f(v.tuple), v.names)
+
+setindex(v::ValuedTuple, value, name) =
+    @closure1 with(v) do tuple
+        setindex(tuple, value, which_index(v, name))
+    end
+
+#DotOverloading.get_field(v::ValuedTuple, value) = getindex(v, value)
 
 """
     Base.merge(v1::ValuedTuple, v2::ValuedTuple)
@@ -139,11 +114,11 @@ Base.getindex(v::ValuedTuple, value) =
 ```jldoctest
 julia> using ValuedTuples
 
-julia> merge((@VT a = 1 b = "b"), (@VT c = 3 d = "d"))
-(@VT a = 1 b = "b" c = 3 d = "d")
+julia> merge(ValuedTuple((1, "b"), @values a b), ValuedTuple((3, "d"), @values c d))
+ValuedTuples.ValuedTuple(a = 1, b = b, c = 3, d = d)
 ```
 """
-Base.merge(v1::ValuedTuple, v2::ValuedTuple) =
+merge(v1::ValuedTuple, v2::ValuedTuple) =
     ValuedTuple((v1.tuple..., v2.tuple...), (v1.names..., v2.names...))
 
 export delete
@@ -153,51 +128,215 @@ export delete
 ```jldoctest
 julia> using ValuedTuples
 
-julia> delete((@VT a = 1 b = 2), @value a)
+julia> delete(ValuedTuple(@kws a = 1 b = 2), @value a)
 (@VT b = 2)
 ```
 """
 delete(v::ValuedTuple, value) = begin
-    names = v.names
-    keeps = map(names) do name
-        not(same_type(name, value))
-    end
-    ValuedTuple(get_index(v.tuple, keeps), get_index(names, keeps))
+    keeps = .!(which_index(v, value))
+    ValuedTuple(v.tuple[keeps], v.names[keeps])
 end
+
+
+value_names(t) = map(fieldtypes(fieldtype(t, :names))) do valtype
+    devalue(valtype)()
+end
+tuple_types(t) = fieldtypes(fieldtype(t, :tuple))
 
 export value_names
 """
-    value_names(t::Type{T}) where T <: ValuedTuple
+    valued_tuple_structure(t)
 
-Extract the names of a valued tuple exclusively based on its type.
+Construct a named tuple of the element types of a named tuple type.
 
 ```jldoctest
 julia> using ValuedTuples
 
-julia> v = @VT a = 1 b = 2;
+julia> t = typeof(ValuedTuple((1, "a"), @values a b));
 
-julia> value_names(typeof(v))
-(:a, :b)
+julia> valued_tuple_structure(typeof(v))
+ValuedTuple(a = Val{Int64}(), b = Val{String}())
 ```
 """
-value_names(t::Type{T}) where T <: ValuedTuple = map(inner_types(fieldtype(t, :names))) do v
-    inner_value(inner_value(v))
-end
+valued_tuple_structure(t) = ValuedTuple(tuple_types(t), value_names(t))
 
-Base.showarray(io::IO, t::Vector{T}, repr::Bool) where T <: ValuedTuple{E, V} where V <: Tuple where E = begin
-    if !repr && get(io, :limit, false) && length(t) > 10
-        t = t[1:10]
+const LeafValuedTuple = ValuedTuple{E, V} where V <: Tuple where E <: Tuple
+
+Base.showarray(io::IO, v::AbstractVector{T}, repr::Bool) where T <: LeafValuedTuple = begin
+    fnames = value_names(T)
+    if !repr && get(io, :limit, false) && length(v) > 10
+        v = v[1:10]
         println(io, "First 10 rows")
     end
-    fnames = value_names(eltype(t))
     show(io,
         Markdown.MD([Markdown.Table(
             unshift!(
-                map(t) do named_tuple
+                map(v) do named_tuple
                     [string.(named_tuple.tuple)...]
                 end,
-                [value_names(eltype(t))...]),
+                [string.(devalue.(fnames))...]),
             repeat([:l], outer = length(fnames)))]))
 end
+
+Base.haskey(v::ValuedTuple, key) = devalue(mapreduce(x -> Val(x == key), |, v.names))
+
+struct AppliedVector{T, F, V} <: AbstractVector{T}
+    afunction::F
+    vector::V
+end
+
+Base.IndexStyle(a::AppliedVector) = IndexLinear()
+Base.size(a::AppliedVector) = size(a.vector)
+Base.getindex(a::AppliedVector, i::Int) = a.afunction(a.vector[i])
+
+AppliedVector{T}(f::F, v::V) where {T, F, V} = AppliedVector{T, F, V}(f, v)
+
+struct GroupedVector{T} <: AbstractVector{T}
+    vector::T
+    windows::Vector{UnitRange{Int64}}
+end
+
+Base.IndexStyle(g::GroupedVector) = IndexLinear()
+Base.size(g::GroupedVector) = size(g.windows)
+Base.getindex(g::GroupedVector, i::Int) = g.vector[g.windows[i]]
+Base.setindex!(g::GroupedVector, values, i::Int) =
+    g.vector[g.windows[i]] .= values
+
+export ZipVector
+struct ZipVector{T, TupleType} <: AbstractVector{T}
+    tuple::TupleType
+end
+
+ZipVector(tuple::T) where T =
+    ZipVector{Tuple{eltype.(tuple)...}, T}(tuple)
+
+getindex_over(vectors, index) =
+    @closure1 map(vectors) do vector
+        vector[index]
+    end
+
+setindex_over!(vectors, values, index) =
+    @closure1 map(vectors, values) do vector, value
+        vector[index] = value
+    end
+
+Base.IndexStyle(e::ZipVector) = IndexLinear()
+Base.size(e::ZipVector) = size(first(e.tuple))
+Base.getindex(e::ZipVector, i::Int) =
+    getindex_over(e.tuple, i)
+Base.setindex!(e::ZipVector, values, i::Int) =
+    setindex_over!(e.tuple, values, i)
+
+struct GroupedZipVector{T} <: AbstractVector{T}
+    tuple::T
+    windows::Vector{UnitRange{Int64}}
+end
+
+GroupedZipVector(tuple::T, windows) where T =
+    GroupedZipVector{T}(tuple, windows)
+
+Base.IndexStyle(g::GroupedZipVector) = IndexLinear()
+Base.size(g::GroupedZipVector) = length(g.windows)
+Base.getindex(g::GroupedZipVector, i::Int) =
+    getindex_over(g.tuple, g.windows[i])
+Base.setindex!(g::GroupedZipVector, values, i::Int) =
+    setindex_over!(g.tuple, values, g.windows[i])
+
+export rowwise
+rowwise(v::ValuedTuple{T, N}) where {T, N} = begin
+    zipped = ZipVector(v.tuple)
+    @closure1 AppliedVector{ValuedTuple{eltype(zipped), N}}(zipped) do row
+        ValuedTuple(row, names)
+    end
+end
+
+export columnwise
+columnwise(v::Vector{T}) where T <: LeafValuedTuple = begin
+    structure = valued_tuple_structure(eltype(v))
+    names = structure.names
+    f = let v = v
+        (val_type, name) -> begin
+            @closure1 AppliedVector{devalue(val_type)}(v) do row
+                row[name]
+            end
+        end
+    end
+    atuple = map(f, structure.tuple, names)
+    ValuedTuple(atuple, names)
+end
+
+export unnest
+unnest(v::ValuedTuple, column) = begin
+    deleted = delete(v, column)
+    tuple_of_vectors = deleted.tuple
+    nested = v[column]
+    outer_length = length(nested)
+    lengths = length.(nested)
+    inner_length = sum(lengths)
+    results = @closure1 map(tuple_of_vectors) do vector
+        @boundscheck @assert length(vector) == outer_length
+        similar(vector, inner_length)
+    end
+    nested_result = Vector{eltype(eltype(nested))}(inner_length)
+    position = 0
+    for i in 1:outer_length
+        start_position = position + 1
+        @inbounds position = position + lengths[i]
+        range = start_position:position
+        @inbounds nested_result[range] .= nested[i]
+        @closure1 map(tuple_of_vectors, results) do vector, result
+            vector[range] = result[i]
+        end
+    end
+    together = ValuedTuple((nested_result, results...), (column, deleted.names...))
+    manyindex(together, v.names)
+end
+
+unnest(v::Vector, column) = begin
+    outer_length = length(v)
+    lengths = map(v) do v
+        length(v[column])
+    end
+    inner_length = sum(lengths)
+    # ugly code to statically generate a return type
+    structure = valued_tuple_structure(eltype(v))
+    edited = setindex(structure, with(eltype, structure[column]), column)
+    result = Vector{ValuedTuple{Tuple{devalue.(edited.tuple)...}, typeof(edited.names)}}(inner_length)
+    position = 0
+    for i in 1:outer_length
+        @inbounds template = v[i]
+        for item in template[column]
+            position = position + 1
+            @inbounds result[position] = setindex(template, item, column)
+        end
+    end
+    result
+end
+
+export order
+order(v::Vector{T}, column) where T <: ValuedTuple =
+    v[sortperm([item[column] for item in v])]
+
+make_windows(vector) = begin
+    alength = length(vector)
+    windows = Vector{UnitRange{Int}}()
+    start_position = 1
+    stop_position = 1
+    current_item = first(vector)
+    for current_position in 2:alength
+        @inbounds next_item = vector[current_position]
+        if next_item != current_item
+            stop_position = current_position - 1
+            push!(windows, start_position:stop_position)
+            start_position = current_position
+        end
+        current_item = next_item
+    end
+    push!(windows, start_position:alength)
+    windows
+end
+
+export group
+group(vector, grouping) = GroupedVector(vector, make_windows(grouping))
 
 end
--- src/tuple_algebra.jl
+++ src/tuple_algebra.jl
@@ -1,10 +1,21 @@
-import Base.tail
+"""
+    @closure1 e::Expr
+
+Applies closure to the first argument of a function. This is useful for do-block
+syntax.
+"""
+macro closure1(e::Expr)
+    if e.head != :call
+        error("Must be a function call")
+    end
+    e.args[2] = :($FastClosures.@closure $(e.args[2]))
+    esc(e)
+end
+
 
 export @value
 """
 ```jdoctest
-julia> using ValuedTuples
-
 julia> @value a
 Val{:a}()
 ```
@@ -16,8 +27,6 @@ end
 export @values
 """
 ```jdoctest
-julia> using ValuedTuples
-
 julia> @values a b
 (Val{:a}(), Val{:b}())
 ```
@@ -28,49 +37,83 @@ macro values(es...)
     end
 end
 
-macro as_value(e)
-    esc(:($Val{$e}()))
+mapfoldr(mapper, reducer, args::Tuple{}) = _empty_reduce_error()
+mapfoldr(mapper, reducer, args::Tuple) =
+    mapfoldr(mapper, reducer, mapper(first(args)), tail(args))
+mapfoldr(mapper, reducer, default, args::Tuple{}) = default
+mapfoldr(mapper, reducer, default, args::Tuple) =
+    reducer(default, mapfoldr(mapper, reducer, mapper(first(args)), tail(args)))
+
+mapreduce(mapper, reducer, args::Tuple) = mapfoldr(mapper, reducer, args)
+mapreduce(mapper, reducer, default, args::Tuple) =
+    mapfoldr(mapper, reducer, default, args)
+
+(&)(v1::Val{true}, v2::Val{true}) = Val{true}()
+(&)(v1::Val, v2::Val) = Val{false}()
+
+(|)(v1::Val{false}, v2::Val{false}) = Val{false}()
+(|)(v1::Val, v2::Val) = Val{true}()
+
+!(::Val{false}) = Val{true}()
+!(::Val{true}) = Val{false}()
+
+ifelse(switch::Val{false}, new, old) = old
+ifelse(switch::Val{true}, new, old) = new
+
+tuple_mismatch_error(t...) = error("Mismatched tuples: leftovers $(t...)")
+
+getindex(into::Tuple{}, index::Tuple{}) = ()
+getindex(into::Tuple, index::Tuple{}) = ()
+getindex(into::Tuple{}, index::Tuple) = ()
+getindex(into::Tuple, index::Tuple) = begin
+    next = getindex(tail(into), tail(index))
+    ifelse(first(index), (first(into), next...), next)
 end
 
-inner_value(::Type{Val{T}}) where T = T
-inner_value(::Val{T}) where T = T
+setindex(old::Tuple{}, new, switch::Tuple{}) = ()
+setindex(old::Tuple{}, new, switch::Tuple) = ()
+setindex(old::Tuple, new, switch::Tuple{}) = old
+setindex(old::Tuple, new, switch::Tuple) =
+    ifelse(first(switch), new, first(old)),
+    setindex(tail(old), new, tail(switch))...
 
-is_value(v::Val{T}) where T = Val{true}()
-is_value(any) = Val{false}()
-
-same_type(a, b) = Val{false}()
-same_type(a::T, b::T) where T = Val{true}()
-
-same_length(v1::Tuple{}, v2::Tuple{}) = Val{true}()
-same_length(v1, v2::Tuple{}) = error("Tuple size mismatch: leftovers $v1")
-same_length(v1::Tuple{}, v2) = error("Tuple size mismatch: leftovers $v2")
-same_length(v1, v2) = same_length(tail(v1), tail(v2))
-
-reduce_tuple(f, default, args) = f(first(args), reduce_tuple(f, default, tail(args)))
-reduce_tuple(f, default, args::Tuple{}) = default
-
-and(v1::Val{true}, v2::Val{true}) = Val{true}()
-and(v1, v2) = Val{false}()
-
-all_tuple(args) = reduce_tuple(and, (@value true), args)
-
-not(::Val{false}) = Val{true}()
-not(::Val{true}) = Val{false}()
-
-if_else(switch::Val{false}, new, old) = old
-if_else(switch::Val{true}, new, old) = new
-
-get_index(into::Tuple{}, index::Tuple{}) = ()
-@inline get_index(into, index) = begin
-    same_length(into, index)
-    next = get_index(tail(into), tail(index))
-    if_else(first(index), (first(into), next...), next)
+setindex(old::Tuple{}, new::Tuple{}, switch::Tuple{}) = ()
+setindex(old::Tuple{}, new::Tuple{}, switch::Tuple) = ()
+setindex(old::Tuple{}, new::Tuple, switch::Tuple{}) = ()
+setindex(old::Tuple{}, new::Tuple, switch::Tuple) = ()
+setindex(old::Tuple, new::Tuple{}, switch::Tuple{}) = old
+setindex(old::Tuple, new::Tuple{}, switch::Tuple) = old
+setindex(old::Tuple, new::Tuple, switch::Tuple{}) = old
+function setindex(old::Tuple, new::Tuple, switch::Tuple)
+    first_switch = first(switch)
+    ifelse(first_switch, first(new), first(old)),
+    setindex(
+        tail(old),
+        ifelse(first_switch, tail(new), new),
+        tail(switch))...
 end
 
-value_wrap(f) = (x, args...) -> @as_value f(inner_value(x), args...)
+devalue(::Val{T}) where T = T
+with(f, v::Val) = Val(f(devalue(v)))
+value_wrap(f) = (x, args...) -> Val(f(devalue(x), args...))
 
-inner_types(any) = inner_types(@as_value any)
-function inner_types(val_type::Val)
+fieldtypes(any) = fieldtypes(Val(any))
+function fieldtypes(val_type::Val)
     Base.@pure inner_function(i) = value_wrap(fieldtype)(val_type, i)
     ntuple(inner_function, value_wrap(nfields)(val_type))
+end
+
+fieldtypes(any) = fieldtypes(Val(any))
+function fieldtypes(val_type::Val)
+    Base.@pure inner_function(i) = with(val_type) do atype
+        fieldtype(atype, i)
+    end
+    ntuple(inner_function, with(nfields, val_type))
+end
+
+function fieldtypes(val_type::Val)
+    Base.@pure inner_function(i) = with(val_type) do atype
+        fieldtype(atype, i)
+    end
+    ntuple(inner_function, with(nfields, val_type))
 end
